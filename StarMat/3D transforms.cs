f2aab126ae4fd1113eefdcb896c32d6be1733467
kmukund87 <devnull@localhost>
2010-04-14 12:50:22 -0500

    First commit of StarMat solution. Consists of Project 1. StartMat 2. TestEXE for StarMat

---
+++ StarMat/3D transforms.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace StarMatLib
+{
+    // note this is set to public for testing purposes only - remove when done.
+    public static partial class StarMat
+    {
+        #region 3D Coordinate Transforms
+
+        public static double[,] translate(double Tx, double Ty, double Tz)
+        {
+            double[,] T = makeIdentity(4);
+
+            T[0, 3] = Tx;
+            T[1, 3] = Ty;
+            T[2, 3] = Tz;
+
+            return T;
+        }
+
+        public static double[,] rotationX(double xdeg)
+        {
+            double[,] ROTX = makeIdentity(4);
+            double xrad;
+            xrad = (Math.PI * xdeg) / 180;
+
+            ROTX[1, 1] = ROTX[2, 2] = Math.Cos(xrad);
+            ROTX[1, 2] = -Math.Sin(xrad);
+            ROTX[2, 1] = Math.Sin(xrad);
+
+            return ROTX;
+        }
+
+        public static double[,] rotationY(double ydeg)
+        {
+            double[,] ROTY = makeIdentity(4);
+            double yrad;
+            yrad = (Math.PI * ydeg) / 180;
+
+            ROTY[0, 0] = ROTY[2, 2] = Math.Cos(yrad);
+            ROTY[2, 0] = -Math.Sin(yrad);
+            ROTY[0, 2] = Math.Sin(yrad);
+
+            return ROTY;
+        }
+
+        public static double[,] rotationZ(double zdeg)
+        {
+            double[,] ROTZ = makeIdentity(4);
+            double zrad;
+            zrad = (Math.PI * zdeg) / 180;
+
+            ROTZ[0, 0] = ROTZ[1, 1] = Math.Cos(zrad);
+            ROTZ[1, 0] = Math.Sin(zrad);
+            ROTZ[0, 1] = -Math.Sin(zrad);
+
+            return ROTZ;
+        }
+        #endregion
+    }
+}
+
---
+++ StarMat/make extract.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace StarMatLib
+{
+    // note this is set to public for testing purposes only - remove when done.
+    public static partial class StarMat
+    {
+        #region Make and Extract functions
+
+        public static int[,] makeZero(int p)
+        {
+            int[,] I = new int[p, p];
+            for (int i = 0; i != p; i++)
+                for (int j = 0; j != p; j++)
+                    I[i, j] = 0;
+            return I;
+        }
+        /// <summary>
+        /// Makes an identity matrix of size p by p.
+        /// </summary>
+        /// <param name="p">The size, p.</param>
+        /// <returns>the identity matrix, I.</returns>
+        public static double[,] makeIdentity(int p)
+        {
+            double[,] I = new double[p, p];
+            for (int i = 0; i != p; i++)
+                for (int j = 0; j != p; j++)
+                    if (i == j) I[i, j] = 1.0;
+                    else I[i, j] = 0.0;
+            return I;
+        }
+        /// <summary>
+        /// Gets a column of matrix, A.
+        /// </summary>
+        /// <param name="colindex">The column index.</param>
+        /// <param name="A">The matrix, A.</param>
+        /// <returns>the column vector, v.</returns>
+        public static double[] GetColumn(int colIndex, double[,] A)
+        {
+            int n = A.GetLength(1);
+            if ((colIndex < 0) || (colIndex >= n))
+                throw new Exception("MatrixMath Size Error: An index value of "
+                    + colIndex.ToString()
+                    + " for getColumn is not in required range from 0 up to (but not including) "
+                    + n.ToString() + ".");
+            else
+            {
+                double[] v = new double[n];
+                for (int i = 0; i < n; i++)
+                    v[i] = A[i, colIndex];
+                return v;
+            }
+        }
+
+        /// <summary>
+        /// Gets a row of matrix, A.
+        /// </summary>
+        /// <param name="colindex">The row index.</param>
+        /// <param name="A">The matrix, A.</param>
+        /// <returns>The index of the row, rowIndex.</returns>
+        public static double[] GetRow(int rowIndex, double[,] A)
+        {
+            int n_rows = A.GetLength(0);
+            int n_cols = A.GetLength(1);
+            if ((rowIndex < 0) || (rowIndex >= n_rows))
+                throw new Exception("MatrixMath Size Error: An index value of "
+                    + rowIndex.ToString()
+                    + " for getRow is not in required range from 0 up to (but not including) "
+                    + n_rows.ToString() + ".");
+            else
+            {
+                double[] v = new double[n_cols];
+                for (int i = 0; i < n_cols; i++)
+                    v[i] = A[rowIndex, i];
+                return v;
+            }
+        }
+
+        /// <summary>
+        /// Sets/Replaces the row of matrix A with the vector v.
+        /// </summary>
+        /// <param name="A">The matrix, A.</param>
+        /// <param name="v">The vecvtor, v.</param>
+        /// <param name="p">The index of the row, rowIndex.</param>
+        public static void SetRow(int rowIndex, double[,] A, double[] v)
+        {
+            int n = A.GetLength(0);
+            if ((rowIndex < 0) || (rowIndex >= n))
+                throw new Exception("MatrixMath Size Error: An index value of "
+                    + rowIndex.ToString()
+                    + " for getRow is not in required range from 0 up to (but not including) "
+                    + n.ToString() + ".");
+            else
+            {
+                for (int i = 0; i < n; i++)
+                    A[rowIndex, i] = v[i];
+            }
+        }
+        public static void SetColumn(int colIndex, double[,] A, double[] v)
+        {
+            int n = A.GetLength(1);
+            if ((colIndex < 0) || (colIndex >= n))
+                throw new Exception("MatrixMath Size Error: An index value of "
+                    + colIndex.ToString()
+                    + " for getColumn is not in required range from 0 up to (but not including) "
+                    + n.ToString() + ".");
+            else
+            {
+                for (int i = 0; i < n; i++)
+                    A[i, colIndex] = v[i];
+            }
+        }
+        #endregion
+    }
+}
+

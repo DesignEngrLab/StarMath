73f5d0fcb32d9ff54d2e41f9bb7e4b3e821b6caa
kmukund87 <devnull@localhost>
2010-04-14 12:58:33 -0500

    Converting name from StarMat to StarMath

---
+++ StarMath/output.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StarMathLib
+{
+    // note this is set to public for testing purposes only - remove when done.
+    public static partial class StarMath
+    {
+        public static void output(object o)
+        {
+
+        }
+        public static void output(object o, int verbosity)
+        {
+
+        }
+    }
+}
+
---
+++ TestEXE for StarMath/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using StarMathLib;
+
+namespace TestEXE_for_StarMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 1000;
+
+            DateTime now = DateTime.Now;
+            Random r = new Random();
+            double[,] A = new double[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    A[i, j] = (200 * r.NextDouble()) - 100.0;
+            Console.WriteLine("start invert check");
+            double[,] B = StarMath.inverse(A);
+            double[,] C = StarMath.subtract(StarMath.multiply(A, B), StarMath.makeIdentity(size));
+            double error = StarMath.norm2(C);
+            TimeSpan interval = DateTime.Now - now;
+            Console.WriteLine("end invert, error = " + error);
+            Console.WriteLine("time = " + interval);
+            Console.ReadLine();
+        }
+    }
+}

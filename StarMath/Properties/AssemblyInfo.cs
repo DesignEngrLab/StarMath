11ec8f5ada418051408999bd2c53583264b123c8
Matt Campbell <mattica@gmail.com>
2014-05-31 10:31:48 -0700

    changed StarMath to a Universal Portable Class Library.That makes things easy!

--- StarMath/Properties/AssemblyInfo.cs
+++ StarMath/Properties/AssemblyInfo.cs
@@ -4,22 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StarMath")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("StarMath: Static Array Math Library")]
+[assembly: AssemblyDescription("Small Library of Vector and Matrix Functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("UT-ADLab")]
+[assembly: AssemblyCompany("Design Engineering Lab -- MICampbell")]
 [assembly: AssemblyProduct("StarMath")]
-[assembly: AssemblyCopyright("Copyright © UT-ADLab 2011")]
+[assembly: AssemblyCopyright("2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d24b97d6-7b40-4bb5-b831-806d40106a83")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +23,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.11.0729")]
-[assembly: AssemblyFileVersion("1.0.11.0729")]
+[assembly: AssemblyVersion("1.0.14.0531")]
+[assembly: AssemblyFileVersion("1.0.14.0531")]

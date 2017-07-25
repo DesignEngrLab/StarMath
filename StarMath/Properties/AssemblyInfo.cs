ac8725be62a12a01b7af62d162deb632ab097ba4
Matt Campbell <mattica@gmail.com>
2017-07-25 15:09:30 -0700

    fixed to pivoting in LU Decomposition and added quick Cramer's rule for matrices of size 2 and 3.

--- StarMath/Properties/AssemblyInfo.cs
+++ StarMath/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.17.0413")]
-[assembly: AssemblyFileVersion("2.0.17.0413")]
+[assembly: AssemblyVersion("2.0.17.0725")]
+[assembly: AssemblyFileVersion("2.0.17.0725")]
